badb2264fb231c89f3cf913b362635faae964428
Justin <jaydom28@gmail.com>
2022-07-17 11:02:58 -0400

    created a function to filter messages by date

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
-from email import message
+from datetime import datetime
+
+from pprint import pprint as pp
+
 from modules import General, Message
+
+
+def filter_messages_by_date(message_objects, month=None, day=None, year=None):
+    """
+    Take in a list of messages and only get the messages that match the desired time
+    """
+    filtered_messages = []
+
+    for msg in message_objects:
+        date_object = datetime.fromtimestamp(msg.time_sent / 1000)
+        msg_day = date_object.strftime('%A')
+        msg_month = date_object.strftime('%B')
+        msg_year = date_object.strftime('%Y')
+
+        if day is not None and day != msg_day:
+            continue
+
+        if month is not None and month != msg_month:
+            continue
+
+        if year is not None and year != msg_year:
+            continue
+
+        filtered_messages.append(msg)
+
+    return filtered_messages
 
 
 def main():
@@ -15,11 +44,22 @@ def main():
 
     # List the participants and the total number of messages per participant
     name_msg_dict = Message.divide_messages_by_sender(message_objects)
-    for sender in name_msg_dict:
+    for sender, messages in name_msg_dict.items():
         print(sender)
-        messages = name_msg_dict[sender]
-        number_messages = len(messages)
-        print(f'  Messages sent: {number_messages}')
+        print(f'  Messages sent: {len(messages)}')
+
+    print('~~~~~~~~~~~~~~~~~~~~~~~')
+    days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    months_of_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                      'August', 'September', 'October', 'November', 'December']
+    total = 0
+    for month in months_of_year:
+        msgs = filter_messages_by_date(message_objects, month=month)
+        num_msgs = len(msgs)
+        print(f'{month}: {num_msgs} messages sent')
+        total += num_msgs
+
+    print(total)
 
 
     # Use the message data to create message objects
